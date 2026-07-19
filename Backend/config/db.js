d91d73e2478f1569da93b02ffaf378186ea45a39
Arjyouhub <arjyou0369@gmail.com>
2026-07-19 16:14:55 +0530

    Support both MONGO_URI and MONGODB_URI

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/spotchat',
+      process.env.MONGO_URI || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/spotchat',
       {
         serverSelectionTimeoutMS: 15000,
       }
